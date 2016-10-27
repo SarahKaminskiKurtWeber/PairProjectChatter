047e099c654efcd01407b88762121ec648807961
sareduhh <sarahe.kaminski@gmail.com>
2016-10-27 10:03:18 -0400

    first commit

---
+++ ChatterPairProgramming/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterPairProgramming.Startup))]
+namespace ChatterPairProgramming
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
